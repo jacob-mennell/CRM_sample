9d859b5cb3f9708df9978df68426e9fcbc87dc04
JacobMennell <jacobmennell@rockborne.com>
2022-06-05 11:00:57 +0100

    swapped new table for view

--- analytical_interviews.sql
+++ analytical_interviews.sql
@@ -1,38 +1,17 @@
-DROP TABLE analytic_first_interview;
+--define view
+CREATE VIEW dbo.analytical_interviews_VIEW AS
 
--- create new analytical table
-CREATE TABLE analytic_first_interview(
-	appointment_id int PRIMARY KEY,
-	candidate_reference_id int,
-	candidate_name varchar(200),
-	email varchar(200),
-	category_name varchar(200),
-	desired_locations varchar(200),
-	type varchar(200),
-	owner_id int,
-	date_begin DATETIME,
-	date_begin_key int,
-	job_submission_id int,
-	job_order_id int,
-	title varchar(200),
-	status varchar(200)
-);
-
---insert data into above table
-INSERT INTO analytic_first_interview (appointment_id, candidate_reference_id,
-candidate_name, email, category_name, desired_locations, type,  owner_id, date_begin, date_begin_key, job_submission_id, job_order_id, title, status)
--- select fields for table
-SELECT
+--select statement
 a.appointment_id,
 a.candidate_reference_id,
-dbo.InitCap(CONCAT(c.first_name,' ', c.last_name)) AS candidate_name, --combine into one name column
+dbo.InitCap(CONCAT(c.first_name,' ', c.last_name)) AS candidate_name, --single name field
 c.email,
 c.category_name,
 c.desired_locations,
 a.type,
 a.owner_id,
 date_begin,
-FORMAT(a.date_begin,'yyyyMMdd') as date_begin_key, --create date key to join date dimension table
+FORMAT(a.date_begin,'yyyyMMdd') as date_begin_key, --join to date dim table
 a.job_submission_id,
 a.job_order_id,
 v.title,
@@ -45,5 +24,5 @@ from test_appointment a
 left join recruiters_dim o on o.owner_id = a.owner_id
 left join test_vacancy v on a.job_order_id = v.vacancy_id
 left join test_candidates c on c.candidate_id = a.candidate_reference_id
-WHERE (UPPER(type) LIKE '%INTERVIEW%' OR UPPER(type) LIKE 'TRIAL%') AND --look at interviews and trial days
+WHERE (UPPER(type) LIKE '%INTERVIEW%' OR UPPER(type) LIKE 'TRIAL%') AND
 candidate_reference_id IS NOT NULL;
